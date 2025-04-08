f0bca8736eeede95ea19cc9ea9637f2acd825511
Vishal Mahana <78928943+vishalmahana@users.noreply.github.com>
2025-04-08 07:53:30 +0000

    Add test data for octofit_db database

---
+++ octofit-tracker/backend/octofit_tracker/test_data.py
@@ -0,0 +1,34 @@
+test_data = {
+    "users": [
+        {"username": "thundergod", "email": "thundergod@mhigh.edu", "password": "thundergodpassword"},
+        {"username": "metalgeek", "email": "metalgeek@mhigh.edu", "password": "metalgeekpassword"},
+        {"username": "zerocool", "email": "zerocool@mhigh.edu", "password": "zerocoolpassword"},
+        {"username": "crashoverride", "email": "crashoverride@mhigh.edu", "password": "crashoverridepassword"},
+        {"username": "sleeptoken", "email": "sleeptoken@mhigh.edu", "password": "sleeptokenpassword"},
+    ],
+    "teams": [
+        {"name": "Blue Team", "members": ["thundergod", "metalgeek"]},
+        {"name": "Gold Team", "members": ["zerocool", "crashoverride", "sleeptoken"]},
+    ],
+    "activities": [
+        {"user": "thundergod", "activity_type": "Cycling", "duration": 3600},
+        {"user": "metalgeek", "activity_type": "Crossfit", "duration": 7200},
+        {"user": "zerocool", "activity_type": "Running", "duration": 5400},
+        {"user": "crashoverride", "activity_type": "Strength", "duration": 1800},
+        {"user": "sleeptoken", "activity_type": "Swimming", "duration": 4500},
+    ],
+    "leaderboard": [
+        {"user": "thundergod", "score": 100},
+        {"user": "metalgeek", "score": 90},
+        {"user": "zerocool", "score": 95},
+        {"user": "crashoverride", "score": 85},
+        {"user": "sleeptoken", "score": 80},
+    ],
+    "workouts": [
+        {"name": "Cycling Training", "description": "Training for a road cycling event"},
+        {"name": "Crossfit", "description": "Training for a crossfit competition"},
+        {"name": "Running Training", "description": "Training for a marathon"},
+        {"name": "Strength Training", "description": "Training for strength"},
+        {"name": "Swimming Training", "description": "Training for a swimming competition"},
+    ],
+}
